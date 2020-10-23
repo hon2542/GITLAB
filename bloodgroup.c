8f756f862c2ae2603b547b821f1444d502e9ea3b
pakwanpch <lawpak2000@gmail.com>
2020-10-23 12:20:52 +0700

    modified bloodgroup by b

--- bloodgroup.c
+++ bloodgroup.c
@@ -2,11 +2,11 @@
 #include <string.h>
 void main()
 
-{ char Name[40], G[5], answer[10];
+{ char Name[60], G[5], answer[10];
   int i;
   do
   {
-    printf("What is your name? ");
+    printf("What's your name? ");
 
     scanf("%s", Name);
     do
